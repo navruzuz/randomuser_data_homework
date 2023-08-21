a5149c4eb123458d1067ef6b5ee3ea8477514de8
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-08-21 06:17:03 +0500

    bajarildi

--- get_data.py
+++ get_data.py
@@ -1,11 +1,16 @@
 import json
 
 def get_data(filename:str) -> dict:
-    """
-    You are given a filename. Read the JSON data from the file and return the dictionary.
+#     """
+#     You are given a filename. Read the JSON data from the file and return the dictionary.
 
-    Args:
-        filename(str): file name
-    Returns:
-        dict: JSON data
-    """
+#     Args:
+#         filename(str): file name
+#     Returns:
+#         dict: JSON data
+#     """
+    f=open('data.json','r')
+    data_json=f.read()
+    data=json.loads(data_json)
+    file_path=json.loads(data)
+    print(get_data(file_path))
